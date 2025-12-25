b489228b9d28335bba54ff9107b6c3ac2c9a1d5d
Tanishka <tanishkatyagi88@gmail.com>
2025-12-26 02:27:21 +0530

    fix: clean project folder structure

--- OneDrive/Desktop/Multischool ID Card System 2/server/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Reusable async handler to wrap route handlers and catch errors
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
